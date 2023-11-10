8bc97fc52a6a3759432e6977354428f7b43d30e0
meulocal <meulocal@gmail.com>
2023-11-10 20:21:26 -0300

    slide 10 persistencia de dados

--- todo_list/lib/models/todo.dart
+++ todo_list/lib/models/todo.dart
@@ -1,6 +1,19 @@
 class Todo {
   Todo({required this.title, required this.dateTime});
 
+  Todo.fromJson(Map<String, dynamic> json)
+    : title = json['title'],
+      dateTime = DateTime.parse(json['datetime']);  
+
   String title;
   DateTime dateTime;
+
+  // criando o MAP
+  Map<String, dynamic> toJson(){
+    return {
+      'title' : title,
+      'datetime': dateTime.toIso8601String(),
+    };
+  }
+  
 }
