1c5486b8b9f996deb74895d90c9b0135e3560dc7
meulocal <meulocal@gmail.com>
2023-11-13 18:58:33 -0300

    adicionando imagem na lista

--- fasesDaLua/lib/pages/lua_pages/luapage.dart
+++ fasesDaLua/lib/pages/lua_pages/luapage.dart
@@ -16,8 +16,9 @@ class LuaPage extends StatelessWidget {
       itemCount: 30,
       itemBuilder: (context, index) {
         return ListTile(
+          leading: Image.asset(_ImageFaseLua(index), height: 50, width: 50),
           title: Text('Data: ${_MonstraData(index)}'),
-          subtitle: Text('fase ${_FaseLua(index)}'),
+          subtitle:  Text('fase ${_FaseLua(index)}'),
         );
       }, separatorBuilder: (context, index) => Divider(),
       
@@ -37,5 +38,27 @@ class LuaPage extends StatelessWidget {
     var lunarDayConway = moon.getLunarDayConway(dateMonth);    
     return 'Fase da lua (provável): ${moon.getMoonPhaseConway(lunarDayConway).name}';
   }
- 
+
+  _ImageFaseLua(int index) {
+    MoonPhase moon=MoonPhase();
+    var dateMonth =  currentDate.add(Duration(days: index));
+    // var lunarDayConway = moon.getLunarDay(dateMonth);
+    var lunarDayConway = moon.getLunarDayConway(dateMonth);    
+    switch(moon.getMoonPhaseConway(lunarDayConway).index) {
+    case 0:
+      return "assets/imagens/luaNova.png";
+    case 1:
+      return "assets/imagens/luaCrescenteConcava.png";
+    case 2:
+      return "assets/imagens/quartoCrescente.png";      
+    case 3:
+      return "assets/imagens/luaCheia.png";
+    case 4:
+      return "assets/imagens/luaMinguanteConvexa.png";
+    case 5:
+      return "assets/imagens/quartoMinguante.png";
+    default:
+      return "assets/imagens/luaMinguanteConcava.png";
+    }
+  }
 }
